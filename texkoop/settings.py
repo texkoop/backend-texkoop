01a1051c4c74b7214500cf082af1f6d7c47f3679
maesterzak <abkdking1703@gmail.com>
2023-01-07 04:24:37 +0100

    add gunicorn

--- texkoop/settings.py
+++ texkoop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1', '.up.railway.app']
 
 
 # Application definition
